10f34fc9923be4999283b7dd0b293da51a61aad2
tiago <tiagojosebogoni@gmail.com>
2019-09-10 23:10:31 -0300

    Criado rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Router} from 'react-router-dom'
+import Routes from './routes'
+
+import history from './services/history';
 
 function App() {
   return (
-   <h1>hello</h1>
+   <Router history={history}>
+     <Routes  />
+   </Router>
   );
 }
 
---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <h1>dashboard</h1>
+  );
+}
