ef4cacd8537fd11726feef51ed0fb391a88faac5
tiagojosebogoni <tiagojosebogoni@gmail.com>
2019-09-16 12:48:40 -0300

    Configurando os redux e saga de autenticação e configurando com o Reactotron

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'reactotron-redux';
 import { Router } from 'react-router-dom';
 import './config/Reactotron';
 
 import Routes from './routes/index';
-import GlobalStyle from './styles/global';
-
 import history from './services/history';
-import { store, persistor } from './store';
+
+import store from './store';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    /* <Provider store={store}>
-      <PersistGate persistor={persistor}> */
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
-    /* </PersistGate>
-    </Provider> */
+     <Provider store={store}>
+
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider> 
   );
 }
 
